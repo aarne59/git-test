ead2c3aac975b2c1e1e5a2b8fa33f54977129468
iivana <aarnet59@gmail.com>
2022-02-02 20:23:08 +0200

    sd

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if user_input.isnumeric():
 # Tämä mainos tulee Visual Studio Codesta
 # Ja tämä
 # 01.02.2022
-# 01.02.2022 / 2 """
+# 01.02.2022 / 2
 
 my_list = ["00", "10", "20", "30", "40", "50"]
 print(my_list)
@@ -29,9 +29,24 @@ from random import random
 
 print(os.name)
 
-
 print('*' * 20)
 
 ika = int(input('anna ikä '))
 ika += 1
-print(f"\nensivuonna ikä on {ika + 1}")
+print(f"\nensivuonna ikä on {ika + 1}")"""
+
+# print('x' * 5)
+"""matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+matrix[1][2] = 20
+print(matrix[1][2])"""
+
+a = [5, 2, 1, 7, 4]
+print(a)
+a.pop()
+print(a)
+a.append('Nisse-2022')
+print(a)
